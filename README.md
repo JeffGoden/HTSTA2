40c691c86646e0429a0a2627e471bff46bedb674
Dan Durbaca <dan.durbaca@education.lu>
2020-03-18 08:56:57 +0100

    prof changes

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 This is an example of a github repository for HTML and CSS
 # This is yet another Title
 This is another description of my project
+
+# I start JS !
+
+Put some description of your own here !
---
+++ first.js
@@ -0,0 +1,16 @@
+function AddSomeThings()
+{
+    let result = 0;
+
+    let myFirstElement = document.getElementById("FirstInput");
+    let mySecoondElement = document.getElementById("SecondInput");
+    // read here :
+    // https://gomakethings.com/converting-strings-to-numbers-with-vanilla-javascript/
+    // and convert myFirstElement.value into a number x
+    // do the same for mySecondElement y
+
+    //add into the result:
+    // result = x + y;
+
+    alert(result);
+}
